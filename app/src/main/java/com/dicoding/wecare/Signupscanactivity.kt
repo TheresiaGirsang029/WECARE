3e90f7836e0fdecf2815c4d7aaff3fbb8d49a698
Theresia Seftiani Girsang <57088138+TheresiaGirsang029@users.noreply.github.com>
2021-06-05 01:36:58 -0700

    git commit -m "pushapp"

---
+++ app/src/main/java/com/dicoding/wecare/Signupscanactivity.kt
@@ -0,0 +1,4 @@
+package com.dicoding.wecare
+
+class Signupscanactivity {
+}
--- app/src/main/java/com/dicoding/wecare/SplashScreen.kt
+++ app/src/main/java/com/dicoding/wecare/SplashScreen.kt
@@ -2,18 +2,17 @@ package com.dicoding.wecare
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 import kotlin.concurrent.schedule
 
-class Spalshscreen : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splashscreen)
 
         Timer("Splash Screen").schedule(3000){
-           val intent = Intent(this@Spalshscreen, MainActivity::class.java)
+           val intent = Intent(this@SplashScreen, MainActivity::class.java)
            startActivity(intent)
            finish()
         }
